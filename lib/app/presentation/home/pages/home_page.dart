63469e08dddef7ba56e31788b0e6e2263821b2e4
Tam Nguyen <nguyenminhtam.developer@gmail.com>
2024-04-01 14:17:38 +0700

    Remove refresh data button

--- lib/app/presentation/home/pages/home_page.dart
+++ lib/app/presentation/home/pages/home_page.dart
@@ -21,12 +21,12 @@ class HomePage extends GetWidget<HomePageController> {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          IconButton(
-            onPressed: () {
-              controller.refreshUser();
-            },
-            icon: const Icon(Icons.refresh_rounded),
-          ),
+          // IconButton(
+          //   onPressed: () {
+          //     controller.refreshUser();
+          //   },
+          //   icon: const Icon(Icons.refresh_rounded),
+          // ),
           IconButton(
             onPressed: () async {
               final GetStorage storage = GetStorage();
